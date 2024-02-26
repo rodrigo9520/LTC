63d64022b97ee64f4910cd2f6025b2e2a6cf9d44
rodrigo9520 <69479098+rodrigo9520@users.noreply.github.com>
2024-02-26 21:15:32 +0000

    New

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+#Ask the user for the path
+read -p "What is the path you want to upload?" path
+
+az storage file upload \ 
+--account-name learntocloud \ 
+--account-key 'oEVjOYnwORHdgHeB+EBVw/ZycHgGRujhGGiGJ6CvlvNLtDqG4m3ReDVB+/IIVJlYZ5h3ayrD6Zqd+AStM12ZLA==' \
+--share-name clouduploader \
+--source $path 
+--path /clouduploader
