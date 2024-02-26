8274f78e82cd747b042a1ebc8321e9f5f18a36b8
rodrigo9520 <69479098+rodrigo9520@users.noreply.github.com>
2024-02-26 21:33:32 +0000

    second

--- clouduploader.sh
+++ clouduploader.sh
@@ -3,9 +3,10 @@
 #Ask the user for the path
 read -p "What is the path you want to upload?" path
 
-az storage file upload \ 
---account-name learntocloud \ 
---account-key 'oEVjOYnwORHdgHeB+EBVw/ZycHgGRujhGGiGJ6CvlvNLtDqG4m3ReDVB+/IIVJlYZ5h3ayrD6Zqd+AStM12ZLA==' \
---share-name clouduploader \
---source $path 
---path /clouduploader
+storage_account="learntocloud"
+storage_key="oEVjOYnwORHdgHeB+EBVw/ZycHgGRujhGGiGJ6CvlvNLtDqG4m3ReDVB+/IIVJlYZ5h3ayrD6Zqd+AStM12ZLA=="
+file_share="clouduploader"
+local_file=$path 
+destination_path="/clouduploader"
+
+azcopy copy $path "https://learntocloud.file.core.windows.net/$file_share$destination_path" --source-key "$storage_key"
