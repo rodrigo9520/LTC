c50f28a9876383d13e1f683df12007ee7ba261cb
rodrigo9520 <69479098+rodrigo9520@users.noreply.github.com>
2024-02-29 19:51:17 +0000

    New file

---
+++ clouduploader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Ask the user for the path
+#read -p "What is the path you want to upload?" path
+
+
+az storage file upload\
+    --account-name "learntocloud"\
+    --account-key "oEVjOYnwORHdgHeB+EBVw/ZycHgGRujhGGiGJ6CvlvNLtDqG4m3ReDVB+/IIVJlYZ5h3ayrD6Zqd+AStM12ZLA=="\
+    --share-name "cloud-uploader"\
+    --source "C:/Users/rodri/LearntoCloud/CapstoneProject/cloudfile.txt"\
+    
+    
+
+
+ 
+
+    
+
+
